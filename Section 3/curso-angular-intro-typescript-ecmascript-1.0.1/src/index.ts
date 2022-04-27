6ba14cd78dadd5d9660f094c200db12245c60f5f
carlos richter <rrch.cr8@gmail.com>
2022-04-27 00:44:09 -0400

    16. Basic Functions

--- Section 3/curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
+++ Section 3/curso-angular-intro-typescript-ecmascript-1.0.1/src/index.ts
@@ -1,8 +1,31 @@
+function sum(a, b){
+ return a+b;
+}
+
+console.log(sum(2, 1)); //makes a sum
+console.log(sum("hola", "perro")); //concatenate
 
 
-console.log('Hola perro!');
+//declaring the parameter types
 
-/*
-    ===== Código de TypeScript =====
-*/
+function sumTyped(a:number, b:number): number{
+ return a+b;
+}
+console.log(sumTyped(2, 1)); //makes a sum
+
+
+//Arrow functions
+
+const arrowSum = (a:number,b:number):number => {
+    return a+b;
+}
+
+//optional values and values per defect
+
+const optionalSum = (a: number, b?: number, c:number=2): number => {
+  return a + c;
+};
+
+console.log(sumTyped(2, 1)); //1 mandatotry and desonc is theoptional
+
 
