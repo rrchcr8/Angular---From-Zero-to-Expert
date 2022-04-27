5833ef72dcb8dcb487e912622cc543f348db1f92
carlos richter <rrch.cr8@gmail.com>
2022-04-26 23:38:21 -0400

    14. basic Types and general concepts

---
+++ Section 3/curso-angular-intro-typescript-ecmascript-1.0.1/src/exercises/01-basic-types.ts
@@ -0,0 +1,14 @@
+let names: string = "stridfer";
+const name2 = "stridfer";
+
+//pipe is type operator that means "or"
+
+let hp : number|string =95
+hp="full"
+
+//boolean
+
+let isAlive: boolean=true;
+
+console.log(names, hp, isAlive);
+
